a3f3d912939769adf9a380161a456ce9f0ba80ed
ValentinNM <valentinm.web@gmail.com>
2021-12-02 10:14:01 +0200

    ADD: Signup Component + MUI dependencies

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,151 @@
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copynotright © "}
+      <Link color="inherit" href="https://mui.com/">
+        tihs is going to be a website name
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignUp({setAuthUser}) {
+
+    const API_URL = process.env.REACT_APP_API_URL
+
+    const [newUser, setNewUser] = useState({
+        email : "", 
+        password : ""
+    })
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const fetchOptions = { 
+        method : "POST",
+        headers : { 
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(newUser) 
+    }
+
+    fetch(`${API_URL}/auth/signup`, fetchOptions)
+    .then((res) => res.json())
+    .catch(error => console.log({error}))
+    .then((data) => { 
+
+        const token = data.token
+
+        if(token){ 
+            setAuthUser(token)
+
+            localStorage.setItem("token", token)
+        }
+    })
+
+  };
+
+
+const handleInput = (event) => { 
+    
+    const name = event.target.name
+    const value = event.target.value
+
+    setNewUser({...newUser, [name] : value})
+    
+}
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={handleInput}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onChange={handleInput}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onSubmit={handleSubmit}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
