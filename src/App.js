86443fd71ce4c59f42035a7a067f4f0d2682554a
ValentinNM <valentinm.web@gmail.com>
2021-12-06 14:38:21 +0200

    Added Dashboard API functionality and 404 page plus MUI DasboardTesting page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import './App.css';
-import {Routes, Route} from "react-router-dom"
-import {useState, useEffect} from 'react';
-import SignUp from './Pages/SignUp';
-import LogIn from './Pages/LogIn';
-import Dashboard from './Pages/Dashboard'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import SignUp from "./Pages/SignUp";
+import LogIn from "./Pages/LogIn";
+import Dashboard from "./Pages/Dashboard";
+import NotFound from "./Pages/NotFound";
 
 export default function App() {
+  
+  const [authUser, setAuthUser] = useState(null);
 
-    const [assets, setAssets] = useState([])
-    const [authUser, setAuthUser] = useState(null)
-
-    return (
-        <div>
-            <main>
-                <Routes>
-                    <Route path="/signup"
-                        element={<SignUp setAuthUser={setAuthUser} />} />
-                    <Route path="/login"
-                        element={<LogIn authUser={authUser} setAuthUser={setAuthUser} />} />
-                    <Route path="/dashboard"
-                        element={<Dashboard authUser={authUser} assets={assets} />} />
-                </Routes>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <main>
+        <Routes>
+          <Route
+            path="/signup"
+            element={<SignUp setAuthUser={setAuthUser} />}
+          />
+          <Route
+            path="/login"
+            element={<LogIn authUser={authUser} setAuthUser={setAuthUser} />}
+          />
+          <Route
+            path="/dashboard"
+            element={<Dashboard authUser={authUser} />}
+          />
+          <Route path="/really-not-found" element={<NotFound />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,60 @@
-export default function Dashboard(props){ 
+import { useState, useEffect } from "react";
 
-    const { } = props;
+export default function Dashboard() {
 
-    return(
-        <div>
-            <h1>
-                code works
-            </h1>
-        </div>
-    )
+  const [stocks, setStocks] = useState([]);
+
+  const STOCKS_API = process.env.REACT_APP_FINNHUB_API;
+  const STOCKS_TOKEN = process.env.REACT_APP_FINN_TOKEN;
+
+  useEffect(() => {
+    fetch(`${STOCKS_API}/stock/peers?symbol=AAPL&token=${STOCKS_TOKEN}`)
+      .then((res) => res.json())
+      .catch(console.error())
+      .then((symbols) => {
+        
+        const cleanSymbols = symbols.filter( symbol => symbol !== '1337.HK');
+
+        console.log({cleanSymbols});
+
+        const symbolFetches = cleanSymbols.map((symbol) => {
+            return fetch(`${STOCKS_API}/quote?symbol=${symbol}&token=${STOCKS_TOKEN}`).then((res) => res.json()).then( prices => ({symbol, prices}))
+            }) 
+
+    Promise.all(symbolFetches).then(data => {
+        console.log({data});
+        setStocks(data);
+      })
+    })
+
+  }, []);
+
+  console.log({stocks});
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        {stocks.map((stock, index) => {
+          const { symbol, prices } = stock;
+          return (
+            <div key={index}>
+              <h2> Stock: {symbol} </h2>
+              <p>current price: ${prices.c}</p>
+              <p>open price: ${prices.o}</p>
+              <p>previous close price: ${prices.pc}</p>
+              <p>day high: ${prices.h}</p>
+              <p>day low: ${prices.l}</p>
+              <div className="stocks-options">
+                <label htmlFor="quantity">
+                  <input type="number" min="0.5" step="0.01" name="quantity" id="quantity" style={{maxWidth : 35 + "px"}} />
+                </label>
+                <button>BUY</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
