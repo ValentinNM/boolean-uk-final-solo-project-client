8dcdf7b16a0638ceed9b56814eeeca3358623393
ValentinNM <valentinm.web@gmail.com>
2021-12-07 14:00:26 +0200

    ADDED: Dashboard-assetToBuy completed + constants.js for APIs

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,35 +1,68 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {  STOCKS_API, STOCKS_TOKEN, CALL_ME_SERVER} from "../utils/constants"
 
 export default function Dashboard() {
-
   const [stocks, setStocks] = useState([]);
+  const [quantityToTrack, setQuantityToTrack] = useState(null);
 
-  const STOCKS_API = process.env.REACT_APP_FINNHUB_API;
-  const STOCKS_TOKEN = process.env.REACT_APP_FINN_TOKEN;
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`${STOCKS_API}/stock/peers?symbol=AAPL&token=${STOCKS_TOKEN}`)
       .then((res) => res.json())
       .catch(console.error())
       .then((symbols) => {
-        
-        const cleanSymbols = symbols.filter( symbol => symbol !== '1337.HK');
-
-        console.log({cleanSymbols});
+        const cleanSymbols = symbols.filter((symbol) => symbol !== "1337.HK");
 
         const symbolFetches = cleanSymbols.map((symbol) => {
-            return fetch(`${STOCKS_API}/quote?symbol=${symbol}&token=${STOCKS_TOKEN}`).then((res) => res.json()).then( prices => ({symbol, prices}))
-            }) 
+          return fetch(
+            `${STOCKS_API}/quote?symbol=${symbol}&token=${STOCKS_TOKEN}`
+          )
+            .then((res) => res.json())
+            .then((prices) => ({ symbol, prices }));
+        });
 
-    Promise.all(symbolFetches).then(data => {
-        console.log({data});
-        setStocks(data);
-      })
-    })
-
+        Promise.all(symbolFetches).then((data) => {
+          setStocks(data);
+        });
+      });
   }, []);
 
-  console.log({stocks});
+  const handleSubmit = (e, stock) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem("token")
+
+    const assetToBuy = {
+      assetSymbol: stock.symbol,
+      price: stock.prices.c,
+      quantity: parseFloat(quantityToTrack)
+    };
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "authorization" : token
+      },
+      body: JSON.stringify(assetToBuy),
+    };
+
+    fetch(`${CALL_ME_SERVER}/trades/buy`, fetchOptions)
+      .then((res) => res.json())
+      .catch(console.error())
+      .then((data) => {
+        const {asset} = data;
+        window.alert(`Congrats 🎉 \n You just bought your ${asset.assetSymbol} stock `)
+      })
+      .catch(console.error());
+  };
+
+  const handleQuantityChange = (e) => {
+    const quantity = e.target.value;
+    setQuantityToTrack(quantity);
+  };
 
   return (
     <div>
@@ -47,9 +80,19 @@ export default function Dashboard() {
               <p>day low: ${prices.l}</p>
               <div className="stocks-options">
                 <label htmlFor="quantity">
-                  <input type="number" min="0.5" step="0.01" name="quantity" id="quantity" style={{maxWidth : 35 + "px"}} />
+                  <input
+                    onChange={handleQuantityChange}
+                    type="number"
+                    min="0.5"
+                    step="0.01"
+                    name="quantity"
+                    id="quantity"
+                    style={{ maxWidth: 40 + "px" }}
+                  />
                 </label>
-                <button>BUY</button>
+                <button type="submit" onClick={(e) => handleSubmit(e, stock)}>
+                  BUY
+                </button>
               </div>
             </div>
           );
