99c8f81a88417d0dfd465050426e8e2e7306f85b
ValentinNM <valentinm.web@gmail.com>
2021-12-15 19:50:13 +0200

    Added: Logic, Filtering and Sorting or the Trades Page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,24 @@
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "../Pages/Dashboard"
+import Dashboard from "../Pages/Dashboard";
 import NotFound from "../Pages/NotFound";
 import Trades from "../Pages/Trades";
+// import TradesTesting from "../Pages/TradesTesting";
 import News from "../Pages/News";
 import Portofolio from "../Pages/Portofolio";
 import Sidemenu from "../Pages/Sidemenu";
+import Account from "../Pages/AccountSettings";
+import EditProfile from "../Pages/EditProfile";
+import { useState } from "react";
 
-export default function Home( props) {
+export default function Home(props) {
+  const { authUser } = props;
 
-    const {authUser} = props;
+  const [showFilter, setShowFilter] = useState(null)
 
   return (
     <>
       <div className="app_main_container">
-        <Sidemenu/>
+        <Sidemenu showFilter={showFilter} />
         <Routes>
           <Route
             path="/dashboard"
@@ -23,9 +28,13 @@ export default function Home( props) {
             path="/portofolio"
             element={<Portofolio authUser={authUser} />}
           />
-          <Route path="/trades" element={<Trades />} />
+          <Route path="/trades" element={<Trades setShowFilter={setShowFilter} />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/profile/edit" element={<EditProfile />} />
+          {/* <Route path="/trades" element={<TradesTesting />} /> */}
           <Route path="/news" element={<News />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />{" "}
+          {/* TODO -> to redirect to NotFound within App.js; so page will be full full browser view*/}
         </Routes>
       </div>
     </>
--- src/Pages/Trades.jsx
+++ src/Pages/Trades.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
-import { CALL_ME_SERVER, STOCKS_API, STOCKS_TOKEN } from "../utils/constants";
+import { CALL_ME_SERVER } from "../utils/constants";
 
 export default function Trades() {
   const [allAssets, setAllAssets] = useState([]);
   const [balance, setBalance] = useState(0);
+  const [symbols, setSymbols] = useState([]);
+  const [filterBySymbol, setFilterBySymbol] = useState([]);
 
   const token = localStorage.getItem("token");
 
@@ -16,70 +18,116 @@ export default function Trades() {
       .then((res) => res.json())
       .catch((error) => console.error({ error }))
       .then((assets) => {
-        const symbols = assets.userAssets.map((asset) => {
-          const { assetSymbol } = asset;
-
-          return fetch(
-            `${STOCKS_API}/quote?symbol=${assetSymbol}&token=${STOCKS_TOKEN}`
-          )
-            .then((res) => res.json())
-            .then((quotes) => {
-              return { ...asset, currentPrices: quotes }; // return is for the fetch
-            });
-        });
-
-        Promise.all(symbols).then((data) => {
-          setAllAssets(data);
-        });
+        setAllAssets(assets.userAssets);
+        setSymbols(assets.userAssets.map((asset) => asset.assetSymbol)); // directly from the userAssets obj extract the assetSymbol only
       })
       .catch((error) => console.error({ error }));
   }, []);
 
+  const handleSort = (e, symbol) => {
+    const type = e.target.type;
+    const checked = e.target.checked;
+
+    if (type === "checkbox") {
+      if (checked) {// handle checked
+
+        const filtered = allAssets.filter((asset) => asset.assetSymbol === symbol); // add one
+
+        setFilterBySymbol(filterBySymbol.concat(filtered.map((asset) => asset.assetSymbol))); // concat || add next obj to array
+      } else { // handle unchecked
+
+        setFilterBySymbol(filterBySymbol.filter((assetSymbol) => assetSymbol !== symbol));
+      }
+    }
+  };
+
+  
+  let filterdAssets = allAssets;
+  
+  if (filterBySymbol.length > 0) {
+    filterdAssets = filterdAssets.filter((asset) =>
+    filterBySymbol.includes(asset.assetSymbol)
+    );
+  }
+
   return (
     <div>
-      <h1>Here's a breakdown of you trades history</h1>
-      <div>
-        <h3> Balance: </h3>
-        <p>
-          {" "}
-          <strong>$</strong> {balance}
-        </p>
-      </div>
-      <hr />
-      <ul>
-        <p>Your Assets</p>
+      <header className="header">
+        <h1>Here's a breakdown of your trades history</h1>
+      </header>
+      <section className="two-column-trades">
+        <main>
+          <table className="table-grid">
+            <colgroup span="5"></colgroup>
+            <tr className="table-columns">
+              <th>Symbol</th>
+              <th>Type</th>
+              <th>Acquiring Price</th>
+              <th> Shares</th>
+              <th>Acquisition Date</th>
+              <th>Total</th>
+            </tr>
 
-        {allAssets.map((asset, index) => {
-          const { assetSymbol, date, price, quantity, currentPrices } = asset;
+            {filterdAssets.map((asset, index) => {
+              const { assetSymbol, date, price, quantity } = asset;
 
-          let total = price * quantity;
+              let total = (price * quantity).toFixed(2);
 
-          // const performance = currentPrices.c * quantity - total;
+              let stringifyDate = new Date(date).toLocaleString();
 
-          let stringifyDate = new Date(date).toLocaleString();
-
-          return (
-            <li key={index}>
-              <hr />
-              <p>
-                <strong> Stock: </strong> {assetSymbol}
-              </p>
-              <p>Total owned: $ {total.toFixed(2)} </p>
-              <p>Acquiring price: $ {price}</p>
-              <p>Shares # {quantity}</p>
-              <p>Acquisition date: {stringifyDate}</p>
-              <p>Current Price: $ {currentPrices.c} </p>
-              <p>Open price: $ {currentPrices.o}</p>
-              <p>previous close price: $ {currentPrices.pc} </p>
-              <p>day high: $ {currentPrices.h} </p>
-              <p>day low: $ {currentPrices.l}</p>
-            </li>
-          );
-        })}
-      </ul>
-      <div className="assets-filter" >
-        <h3>Filter by:</h3>
-      </div>
+              return (
+                <tr className="table-columns" key={index}>
+                  <th className="table-cell"> {assetSymbol} </th>
+                  <th className="table-cell"> {asset.type} </th>
+                  <th className="table-cell"> $ {price} </th>
+                  <th className="table-cell"> {quantity} </th>
+                  <th className="table-cell"> {stringifyDate} </th>
+                  <th className="table-cell"> $ {total} </th>
+                </tr>
+              );
+            })}
+          </table>
+        </main>
+        <aside className="trades-aside">
+          <div>
+            <h3> Balance: </h3>
+            {/* TODO -> display balance in raport with the Accound model */}
+            <p>
+              <strong>$</strong> {balance}
+            </p>
+            <div>
+              <input type="date" name="date" onChange={handleSort} />
+              <select name="dropdown" onChange={handleSort}>
+                <option value=""> Please select ...</option>
+                <option value="asc" name="asc">
+                  Ascending
+                </option>
+                <option value="desc" name="desc" onSelect={handleSort}>
+                  Descending
+                </option>
+              </select>
+            </div>
+          </div>
+          <div className="assets-filter">
+            <h3>Filter by:</h3>
+            {symbols.map((symbol, index) => {
+              // TODO - display unique symbol
+              return (
+                <div key={index} className="symbol-filter">
+                  <input
+                    type="checkbox"
+                    name={symbol}
+                    value={symbol}
+                    onClick={(e) => handleSort(e, symbol)}
+                  />
+                  <label htmlFor=""> {symbol} </label>
+                </div>
+              );
+            })}
+            <button type="button" name="clear" className="clear-all">Clear All</button>
+          </div>
+        </aside>
+      </section>
     </div>
   );
 }
